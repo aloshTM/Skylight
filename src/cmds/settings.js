9ce832696bf06c5d2ce35500446129bfae25e112
aloshTM <alosh@alosh.shop>
2024-02-25 04:54:56 -0600

    I cooked right guys

--- src/cmds/settings.js
+++ src/cmds/settings.js
@@ -1,14 +1,26 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('settings')
         .setDescription('Configure the settings of this bot.'),
     async execute(interaction) {
-        await interaction.reply(":thinking:")
-        let settingsEmbed = new EmbedBuilder()
-            .setTitle(":wrench: Settings")
-            .setDescription(":")
-        interaction.followUp({ embeds: [settingsEmbed] })
+        await interaction.reply(":thinking:");
+        fs.readFile('settings.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const settings = JSON.parse(data);
+            const emoji = settings.verbose ? ':white_check_mark:' : ':x:';
+            const description = settings.verbose ? 'Allow the bot to reply detailed messages of what happened instead of just emojis.' : 'Verbose messages are disabled.';
+
+            let settingsEmbed = new EmbedBuilder()
+                .setTitle(":wrench: Settings")
+                .setDescription(`${emoji} **Verbose Messages**\n${description}`);
+
+            interaction.followUp({ embeds: [settingsEmbed] });
+        });
     }    
-}
+};
