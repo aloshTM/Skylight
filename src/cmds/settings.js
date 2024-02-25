a7d95264a56e9dcbda0ef14845e1688b06ef83a3
aloshTM <alosh@alosh.shop>
2024-02-25 04:56:08 -0600

    o

--- src/cmds/settings.js
+++ src/cmds/settings.js
@@ -10,17 +10,22 @@ module.exports = {
         fs.readFile('settings.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
+                interaction.followUp('Failed to read settings. Please try again later.');
                 return;
             }
-            const settings = JSON.parse(data);
-            const emoji = settings.verbose ? ':white_check_mark:' : ':x:';
-            const description = settings.verbose ? 'Allow the bot to reply detailed messages of what happened instead of just emojis.' : 'Verbose messages are disabled.';
-
-            let settingsEmbed = new EmbedBuilder()
-                .setTitle(":wrench: Settings")
-                .setDescription(`${emoji} **Verbose Messages**\n${description}`);
-
-            interaction.followUp({ embeds: [settingsEmbed] });
+            try {
+                const settings = JSON.parse(data);
+                const emoji = settings.verbose ? ':white_check_mark:' : ':x:';
+                const description = 'Allow the bot to reply detailed messages of what happened instead of just emojis.'
+    
+                let settingsEmbed = new EmbedBuilder()
+                    .setTitle(":wrench: Settings")
+                    .setDescription(`${emoji} **Verbose Messages**\n${description}`);
+                interaction.followUp({ embeds: [settingsEmbed] });
+            } catch (error) {
+                console.error(error);
+                interaction.followUp('Failed to parse `settings.json`. Please contact the guild owner to fix this.');
+            }
         });
     }    
 };
