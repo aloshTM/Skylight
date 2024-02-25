ec304dd6370c5212b3181e1cf94216e94c9577e4
aloshTM <alosh@alosh.shop>
2024-02-25 03:56:48 -0600

    node project init and index took from old project

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
+require('dotenv').config();
+
+const token = process.env.DISCORD_TOKEN;
+const appId = process.env.APP_ID;
+const guildId = process.env.GUILD_ID;
+
+// console.log(token);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+		commands.push(command.data);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`[ERROR] No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+		else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.once(Events.ClientReady, c => {
+	console.log(`[LOG] Ready! Logged in as ${c.user.tag}`);
+});
+
+const rest = new REST().setToken(token);
+
+(async () => {
+	try {
+		console.log(`[LOG] Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(
+			Routes.applicationGuildCommands(appId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`[LOG] Successfully reloaded ${data.length} application (/) commands.`);
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
+
+client.login(token);
