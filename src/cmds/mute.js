2c01f6ae6b47081b20c7ec4e180b300b6b2aebf2
aloshTM <alosh@alosh.shop>
2024-02-25 04:04:53 -0600

    Dont Work Debug Time

--- src/cmds/mute.js
+++ src/cmds/mute.js
@@ -11,6 +11,7 @@ module.exports = {
                     .setRequired(true)),
     async execute(interaction) {
         const user = interaction.options.getUser('user')
+        console.log(user)
         interaction.reply(user)
     }    
 }
